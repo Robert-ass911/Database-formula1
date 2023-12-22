25dfa82801db975417e09e1bad9018000a4e306c
Robert-ass911 <112751073+Robert-ass911@users.noreply.github.com>
2023-12-23 01:40:56 +0300

    Add files via upload

---
+++ src/database/models.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class Races(BaseModel):
+    ID : int
+    Race_Name : str
+    Date : datetime
+    Location : str
+
+class Teams(BaseModel):
+    ID : int
+    Team_Name : str
+    Team_Principal : str
+
+class Drivers(BaseModel):
+    ID : int
+    Team_ID : int
+    Driver_Name : str
+
+class PitStops(BaseModel):
+    ID : int
+    Race_ID : int
+    Driver_ID : int
+    Pitstop_Time : datetime
+
+class Laps(BaseModel):
+    ID : int
+    Race_ID : int
+    Driver_ID : int
+    Lap_Time : datetime
+
+class Standings(BaseModel):
+    ID : int
+    Race_ID : int
+    Driver_ID : int
+    Position : int
+    Points : int
