95e19be66d6e4cd7ac875d2a57029cf5b11092fb
Robert-ass911 <112751073+Robert-ass911@users.noreply.github.com>
2023-12-24 13:01:57 +0300

    Add files via upload

---
+++ src/server/routers/Users.py
@@ -0,0 +1,32 @@
+import fastapi
+
+from database.models import Users
+from resolvers import Users_res
+
+
+Users_router = fastapi.APIRouter(prefix='/Users', tags=["Users"])
+
+
+@Users_router.get(path='/get/{Users_ID}', response_model=dict)
+def get_User(Users_ID: int) -> dict:
+    return Users_res.get(User_ID = Users_ID)
+
+@Users_router.get(path='/get', response_model=dict)
+def get_Users() -> dict:
+    return Users_res.get_all()
+
+@Users_router.post(path='/new', response_model=dict)
+def new_User(User: Users) -> dict:
+    return Users_res.new(User = User)
+
+@Users_router.put(path='/updatePassword/{Users_ID}', response_model=dict)
+def update_Password(User: Users) -> dict:
+    return Users_res.update_Password(User = User)
+
+@Users_router.put(path='/updateLogin/{Users_ID}', response_model=dict)
+def update_Login(User: Users) -> dict:
+    return Users_res.update_Login(User = User)
+
+@Users_router.delete(path='/delete/{Users_ID}', response_model=dict)
+def delete_User(User_ID: int) -> dict:
+     return Users_res.delete(User_ID = User_ID)
