4eb25ae3888bc6f53e11b9ceacd953d405a79af1
Robert-ass911 <112751073+Robert-ass911@users.noreply.github.com>
2023-12-24 12:24:46 +0300

    Delete src directory

--- src/settings.py
+++
@@ -1,4 +0,0 @@
-DB_PATH = 'C:/Users/samar/PycharmProjects/formula/database/formula.db'
-SCRIPTS_TABLES_PATH = 'C:/Users/samar/PycharmProjects/formula/database/scripts/base.sql'
-HOST = '127.0.0.1'
-PORT = 8000
